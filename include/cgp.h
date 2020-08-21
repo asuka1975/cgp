81ec710054e855c95a1d8074626e669eb6bc74e4
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2020-08-22 06:43:51 +0900

    create base of cgp

--- include/cgp.h
+++ include/cgp.h
@@ -5,4 +5,26 @@
 #ifndef CGP_CGP_H
 #define CGP_CGP_H
 
+#include <cstdint>
+#include <functional>
+#include <vector>
+#include "genetic.h"
+#include "cgp_genome.h"
+
+struct cgp_config {
+    std::uint32_t input_num;
+    std::uint32_t output_num;
+    std::uint32_t node_input_num;
+    std::pair<std::uint32_t, std::uint32_t> network_size;
+    std::vector<std::function<float(float)>> f;
+};
+
+template <std::size_t I, class... TArgs>
+void configure_cgp(const cgp_config& config, genetic::ga_config<TArgs...>& gconfig) {
+    static_assert(I < std::tuple_size_v<std::tuple<TArgs...>>, "index out of range of parameter pack `TArgs...`");
+    static_assert(std::is_same_v<std::tuple_element_t<I, std::tuple<TArgs...>>, cgp_genome>,
+                  "selected type should be cgp_genome");
+
+}
+
 #endif //CGP_CGP_H
---
+++ src/cgp_genome.cpp
@@ -0,0 +1,8 @@
+//
+// Created by hungr on 2020/08/13.
+//
+#include "cgp_genome.h"
+
+cgp_genome cgp_genome::crossover(const cgp_genome &d1, const cgp_genome &d2) {
+    return d1;
+}
